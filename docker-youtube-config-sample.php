f81a9c7647bdaccdea716e2c44621d97f4d3c5db
Adam Maracz <adam.maracz@me.com>
2021-04-17 18:05:20 +0200

    Create docker-youtube-config-sample.php

---
+++ docker-youtube-config-sample.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'api_key' => '<YOUTUBE API KEY>', //See https://console.developers.google.com/apis/api/youtube.googleapis.com/
+	'file_dir' => '/downloads/',
+	'client_key' => 'webos2009060606',	//If you want some security, replace this with a random string here and in debug_key. Your client must know this value.
+	'debug_key' => 'webos2009060606',	//If you want some security, replace this with a random string here and in client_key. Your client must know this value.
+	'server_id' => 'webos2009060606'	//If you want some more security, replace this with a random string here. Your client must know this value.
+);
+?>
